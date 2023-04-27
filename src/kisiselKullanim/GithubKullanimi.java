4b2aac32012f2ac63c3a25c13524373906aa3e90
muhammetali35 <129202058+muhammetali35@users.noreply.github.com>
2023-04-27 15:55:03 +0300

    ilk değişiklik

--- src/kisiselKullanim/GithubKullanimi.java
+++ src/kisiselKullanim/GithubKullanimi.java
@@ -3,5 +3,7 @@ package kisiselKullanim;
 public class GithubKullanimi {
     public static void main(String[] args) {
         System.out.println("Hello World MuhammetAli");
+
+        System.out.println("That's it");
     }
 }
